5d45424c62c2fbdf6ae16eced7b5f5817886bc86
bahiyanaa <74060646+bahiyanaa@users.noreply.github.com>
2020-11-07 17:07:50 +0500

    Update pong.py

--- pong.py
+++ pong.py
@@ -44,7 +44,8 @@ pen.color("red")
 pen.penup()
 pen.hideturtle()
 pen.goto(0, 260)
-pen.write("Player A: 0 Player B: 0", align='center', font=("Courier", 24, "normal"))
+pen.write("Player A: 0 Player B: 0", align='center',
+          font=("Courier", 24, "normal"))
 
 
 def paddle_a_up():
@@ -94,19 +95,22 @@ while True:
         ball.dx *= -1
         score_a += 1
         pen.clear()
-        pen.write("Player A: {} Player B: {}".format(score_a, score_b), align='center', font=("Courier", 24, "normal"))
+        pen.write("Player A: {} Player B: {}".format(score_a, score_b),
+                  align='center', font=("Courier", 24, "normal"))
 
     if ball.xcor() < -390:
         ball.goto(0, 0)
         ball.dx *= -1
         score_b += 1
         pen.clear()
-        pen.write("Player A: {} Player B: {}".format(score_a, score_b), align='center', font=("Courier", 24, "normal"))
+        pen.write("Player A: {} Player B: {}".format(score_a, score_b),
+                  align='center', font=("Courier", 24, "normal"))
+    if (340 < ball.xcor() < 350) and (paddle_b.ycor() + 50 >
+       ball.ycor() > paddle_b.ycor() - 50):
+            ball.setx(340)
+            ball.dx *= -1
 
-    if (340 < ball.xcor() < 350) and (paddle_b.ycor() + 50 > ball.ycor() > paddle_b.ycor() - 50):
-        ball.setx(340)
-        ball.dx *= -1
-
-    if (-340 > ball.xcor() > -350) and (paddle_a.ycor() + 40 > ball.ycor() > paddle_a.ycor() - 40):
-        ball.setx(-340)
-        ball.dx *= -1
+    if (-340 > ball.xcor() > -350) and (paddle_a.ycor() + 40 >
+       ball.ycor() > paddle_a.ycor() - 40):
+            ball.setx(-340)
+            ball.dx *= -1
